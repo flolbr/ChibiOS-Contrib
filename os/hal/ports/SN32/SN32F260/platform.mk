be7c7580b58f87b2069e73b05eb4a004439e8265
toast <toast@killthought.com>
2020-03-07 17:21:57 -0500

    fix build error on windows fix linker error missing sbrk

--- os/hal/ports/SN32/SN32F260/platform.mk
+++ os/hal/ports/SN32/SN32F260/platform.mk
@@ -1,6 +1,7 @@
 # Required platform files.
 PLATFORMSRC := $(CHIBIOS)/os/hal/ports/common/ARMCMx/nvic.c \
-               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F260/hal_lld.c
+               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F260/hal_lld.c \
+			   $(CHIBIOS)/os/various/syscalls.c \
 
 # Required include directories.
 PLATFORMINC := $(CHIBIOS)/os/hal/ports/common/ARMCMx \
